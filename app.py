2010e94690b220b7b95b83e4d339406355ddfd51
notacat1221 <rs.frere@gmail.com>
2024-07-23 18:01:54 +0100

    Added navigation through sidebar, file and directory downloading

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 
@@ -10,7 +10,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 
-import os
+from werkzeug.utils import secure_filename
+
+import os, zipfile
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 BASE_DIR = 'share'
@@ -73,20 +75,17 @@ class RegisterForm(FlaskForm):
 #Ensure that user is properly authenticated with valid session data
 @app.before_request
 def require_login():
-    if 'email' not in session and request.endpoint not in ['login', 'register']: # dont check authentication for login and register pages as unauthenticated users must still access
+    if 'id' not in session and request.endpoint not in ['login', 'register']: # dont check authentication for login and register pages as unauthenticated users must still access
         return redirect(url_for('login'))
 
 @app.route('/')
 def landingZone():  # put application's code here
-    if 'email' in session:
-        return redirect(url_for('listFiles'))
+    return render_template('index.html')
 
 
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    if 'email' in session:
-        return redirect(url_for('landingZone'))
     form = LoginForm()
     if form.validate_on_submit(): #Checks that submitted login form adheres to input validation rules
         email = form.email.data
@@ -94,7 +93,8 @@ def login():
 
         user = User.query.filter_by(email=email).first()
         if user and bcrypt.check_password_hash(user.hashedPass, password): #Check stored password hash against input using bcrypt, ensure it matches to the correct user
-            session['email'] = email #if user validated successfully, set email in session data so that user remains signed in
+            session['id'] = user.id #if user validated successfully, set user id in session data so that user remains signed in
+            return redirect(url_for('landingZone'))
         else:
             flash('Invalid credentials', 'error')
     return render_template('login.html', form = form)
@@ -110,51 +110,67 @@ def register():
         user = User(email=email, hashedPass=hashedPass)
 
         try:
-            db.session.add(user)
-            db.session.commit()
+            db.session.add(user) #Add user to database
+            db.session.commit() #Commit changes
             flash('You have successfully registered! Now log in', 'success')
             return redirect(url_for('login'))
-        except IntegrityError:
+        except IntegrityError: #CHANGE
             db.session.rollback()
             flash('Email already registered', 'error')
     return render_template('register.html', form = form)
 
 @app.route('/logout')
 def logout():
-    session.pop('email', None)
+    session.pop('id', None) #Pop users email from session, effectively logging them out as this data is used to verify access
     return render_template('logout.html')
 
 #Filepath routing
 @app.route('/share', defaults={'subpath': ''})
 @app.route('/share/<path:subpath>')
-def listFiles(subpath):
+def share(subpath):
     # Normalize the subpath to remove any trailing slashes
     subpath = subpath.rstrip('/')
     fullPath = os.path.join(BASE_DIR, subpath)
 
     if not os.path.exists(fullPath):
         flash('No such directory found')
-        return redirect(url_for('listFiles'))
-
+        return redirect(url_for('share'))
+    #Calculate parent path of the currently accessed directory
     parentPath = os.path.dirname(subpath)
-    items = []
 
-    try:
-        with os.scandir(fullPath) as currentDir:
-            for entry in currentDir:
-                try:
-                    items.append({
-                        'name': entry.name,
-                        'is_dir': entry.is_dir(),
-                        'path': os.path.join(subpath, entry.name)
-                    })
-                except PermissionError:
-                    flash(f'Permission denied on {entry.name}.', 'error')
-    except PermissionError:
-        flash('Permission denied for the directory.', 'error')
+    items = []
+    with os.scandir(fullPath) as currentDir:
+        for entry in currentDir:
+            try:
+                items.append({
+                    'name': entry.name,
+                    'is_dir': entry.is_dir(),
+                    'path': os.path.join(subpath, entry.name)
+                })
+            except PermissionError: #Any protected files or within the directory will not be accessible, instead the following message will appear
+                flash(f'Permission denied on {entry.name}.', 'error')
 
     return render_template('share.html', items=items, parentPath=parentPath, subpath=subpath)
+@app.route('/download/<path:subpath>')
+def download(subpath):
+    subpath = subpath.rstrip('/') #Strip trailing /
+    fullPath = os.path.join(BASE_DIR, subpath)
+    #If path is a directory, archive before sending
+    if os.path.isdir(fullPath):
+        zipName = f"{secure_filename(subpath)}.zip"
+        zipPath = os.path.join(BASE_DIR, zipName)
 
+        #Create zip archive
+        with zipfile.ZipFile(zipPath, 'w', zipfile.ZIP_DEFLATED) as zippedFile:
+            for root, dirs, files in os.walk(fullPath): #Recursively search directory selected for archival
+                for file in files:
+                    filePath = os.path.join(root, file)
+                    zippedFile.write(filePath, os.path.relpath(filePath, BASE_DIR))
+        response = send_file(zipPath, as_attachment=True)
+        os.remove(zipPath)
+    else:
+        response = send_file(fullPath, as_attachment=True)
+    return response
 
 if __name__ == '__main__':
     app.run()
